998b640ae9de70284ccdfb790df0d1700a2691cd
Sebastiano Laurent <sebastiano.laurent@outlook.com>
2026-04-14 12:47:19 +0200

    chore: update production MapKit token configuration

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
   const localhostToken = '';
 
   // Token valido per il dominio pubblico autorizzato.
-  const productionToken = 'eyJraWQiOiJVWFQzUDkyWEczIiwidHlwIjoiSldUIiwiYWxnIjoiRVMyNTYifQ.eyJpc3MiOiJEOFVSSjk3UEZQIiwiaWF0IjoxNzc2MTUxMzM3LCJvcmlnaW4iOiJzZWJhc3RpYW5vbGF1cmVudC5naXRodWIuaW8iLCJzY29wZSI6Im1hcGtpdF9qcyJ9.XxvrGKPNF5T7DI-scRCZ5_v0xpwNUAEsntKWgdJk6nLNN2P1r_yyfgcLMjM-FhUrei90fVY583dckDB9XFFd5Q';
+  const productionToken = 'eyJraWQiOiJCUVU1VkxHV1ZYIiwidHlwIjoiSldUIiwiYWxnIjoiRVMyNTYifQ.eyJpc3MiOiJEOFVSSjk3UEZQIiwiaWF0IjoxNzc2MTYzMzY4LCJvcmlnaW4iOiJsdWNhZWxlb25vcmEuY29tIiwic2NvcGUiOiJtYXBraXRfanMifQ.NMZiB1QwZHLdRG1L4KLuURXwF9MOZnk87RgO7_K28K3pD2pgs89j70-pY7-C7v58YZvVtLUCb4J1UKsRQGWQlw';
 
   window.MAPKIT_JWT = isLocalhost ? localhostToken : productionToken;
 })();
