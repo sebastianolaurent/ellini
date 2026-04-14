18b76437280c277d0a1a99a09bf56415737adbc8
Sebastiano Laurent <sebastiano.laurent@outlook.com>
2026-04-14 09:34:30 +0200

    chore: remove obsolete MapKit token template file

--- config.example.js
+++
@@ -1,3 +0,0 @@
-// Copia questo file in config.js e inserisci un token JWT MapKit JS valido.
-// Il token deve essere generato dal tuo account Apple Developer.
-window.MAPKIT_JWT = 'INSERISCI_QUI_IL_TOKEN_MAPKIT_JWT';
