a221b9a63c8746a22421aa1e9b174df03056adbe
Sebastiano Laurent <sebastiano.laurent@outlook.com>
2026-04-14 14:40:13 +0200

    chore: add Supabase client configuration for guest photos

--- config.js
+++ config.js
@@ -9,4 +9,10 @@
   const productionToken = 'eyJraWQiOiJCUVU1VkxHV1ZYIiwidHlwIjoiSldUIiwiYWxnIjoiRVMyNTYifQ.eyJpc3MiOiJEOFVSSjk3UEZQIiwiaWF0IjoxNzc2MTYzMzY4LCJvcmlnaW4iOiJsdWNhZWxlb25vcmEuY29tIiwic2NvcGUiOiJtYXBraXRfanMifQ.NMZiB1QwZHLdRG1L4KLuURXwF9MOZnk87RgO7_K28K3pD2pgs89j70-pY7-C7v58YZvVtLUCb4J1UKsRQGWQlw';
 
   window.MAPKIT_JWT = isLocalhost ? localhostToken : productionToken;
+
+  window.SUPABASE_CONFIG = {
+    url: 'https://edontmlcteelyjrqxnzf.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkb250bWxjdGVlbHlqcnF4bnpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxNTgzODEsImV4cCI6MjA5MTczNDM4MX0.yPcp_tj1CIyICpH4-Ik-HiAx7o6HByCNgPiEQ44MjRU',
+    bucket: 'wedding-photos'
+  };
 })();
