31ddf9bebbb5ff116b41e31dfc74c30e3b3807ef
Sebastiano Laurent <sebastiano.laurent@outlook.com>
2026-04-16 12:06:35 +0200

    chore: add wedding event window configuration

--- config.js
+++ config.js
@@ -15,4 +15,13 @@
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkb250bWxjdGVlbHlqcnF4bnpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxNTgzODEsImV4cCI6MjA5MTczNDM4MX0.yPcp_tj1CIyICpH4-Ik-HiAx7o6HByCNgPiEQ44MjRU',
     bucket: 'wedding-photos'
   };
+
+  window.WEDDING_EVENT_CONFIG = {
+    timezone: 'Europe/Rome',
+    weddingDate: '2026-09-26',
+    galleryWindow: {
+      startDate: '2026-09-24',
+      endDate: '2026-09-29'
+    }
+  };
 })();
